bcb0d29d7b07849b367cac8954efa996a899b464
JesusT <core.jesust@gmail.com>
2020-09-09 10:56:12 +0200

    Generada actividad Info y lógica btn Info y Salir

---
+++ app/src/main/java/com/example/rocas/InfoActivity.java
@@ -0,0 +1,19 @@
+package com.example.rocas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class InfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info);
+    }
+
+    public void volver(View view){
+        finish();
+    }
+}
