4f1a56005073cddf9a0921fbf2b816313b3d5269
JesusT <core.jesust@gmail.com>
2020-09-11 11:30:54 +0200

    Agregado menú y imagen de fondo

--- app/src/main/java/com/example/rocas/MainActivity.java
+++ app/src/main/java/com/example/rocas/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.rocas;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +18,36 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    // Creando y configurando menú
+    @Override
+    public boolean onCreateOptionsMenu(Menu miMenu) {
+        getMenuInflater().inflate(R.menu.menu, miMenu);
+        return true; //super.onCreateOptionsMenu(miMenu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id=item.getItemId();
+        boolean resp = true;
+        switch (id){
+            case R.id.btn_info:
+                openActivityInfo(null);
+                break;
+            case R.id.btn_setting:
+                dontExist(null); // lo cambiaremos cuando lo hagamos
+                break;
+            case R.id.btn_play:
+                dontExist(null); // lo cambiaremos cuando lo hagamos
+                break;
+            case R.id.btn_exit:
+                closeApp(null);
+            default:
+                resp = false;
+        }
+        return resp;
+    }
+
+    // Funcionalidad de los botones
     public void openActivityInfo(View view){
         Intent activInfo = new Intent(this, InfoActivity.class);
         startActivity(activInfo);
@@ -21,5 +55,9 @@ public class MainActivity extends AppCompatActivity {
 
     public void closeApp(View view){
         finish();
+    }
+
+    public void dontExist(View view){
+        Toast.makeText(this, R.string.opt_dont_exist,Toast.LENGTH_LONG).show();
     }
 }
